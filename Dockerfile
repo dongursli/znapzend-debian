6b036ee45758eee16ae17cd6dbf8b89f43aa4d5d
dongursli <20471705+dongursli@users.noreply.github.com>
2025-12-05 06:57:44 +0100

    Fixed errors, bumped version and changed to podman

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,12 @@
 ARG image=debian:latest
-ARG znapzend-tag=v0.21.1
+ARG znapzend-tag=v0.23.2
 
 FROM ${image}
 
 RUN apt-get update && apt-get install -y git devscripts debhelper unzip build-essential
-RUN git clone -b v0.21.1 https://github.com/oetiker/znapzend
+RUN git clone -b v0.23.2 https://github.com/oetiker/znapzend
 
+RUN rm -rf znapzend/debian
 COPY debian znapzend/debian
 
 RUN cd znapzend && debuild --no-tgz-check -us -uc
--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 
-znapzend_tag=0.21.1
+znapzend_tag=0.23.2
 docker_tag=debian
 
 
-docker build -t znapzendbuild --build-arg=image=$docker_tag --build-arg=znapzend-tag=$znapzend_tag .
-docker run -v $PWD:/opt/workdir --rm znapzendbuild bash -c "cp /znapzend_*.deb /opt/workdir"
+podman build -t znapzendbuild --build-arg=image=$docker_tag --build-arg=znapzend-tag=$znapzend_tag .
+podman run -v $PWD:/opt/workdir --rm znapzendbuild bash -c "cp /znapzend_*.deb /opt/workdir"
 
